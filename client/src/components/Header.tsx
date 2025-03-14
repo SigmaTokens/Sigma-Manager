0a02122afb510d2220e447cb582b43cfc408d1ec
Daniel Nagornov <Danieln056@gmail.com>
2025-03-14 16:17:45 +0200

    Add production & development different scripts

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,17 @@
 
+import "../styles/Header.css"
+
 function Header(){
-    return <h1>Header</h1>
+    
+    return ( 
+    <header className="header">
+        <nav>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Alerts</a></li>
+        </nav>
+        <h1>Welcome</h1>
+    </header> 
+    )
 }
 
 export default Header

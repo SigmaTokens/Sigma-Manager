446d65eaeb87f22612e6759d2b36ae6a092bd34c
Daniel Nagornov <Danieln056@gmail.com>
2025-03-14 16:42:18 +0200

    move serving client-files to its own file

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,11 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import { serveClient } from "./routes/client";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const clientBuildPath = path.join(__dirname, '../client/dist');
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(clientBuildPath));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(clientBuildPath, 'index.html'));
-  });
-} else {
-
-  app.get('*', (req, res) => {
-    res.send("API is running in development mode. Use Vite for the client.");
-  });
-}
+serveClient(app);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });

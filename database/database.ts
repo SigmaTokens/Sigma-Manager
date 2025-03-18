38c16eda9bc3aa67fe3cccf163652520b1e722f0
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2025-03-18 15:42:21 +0200

    init honeytokens table

--- database/database.ts
+++ database/database.ts
@@ -1,12 +1,32 @@
 import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import { open, Database } from "sqlite";
+
+async function init_honeytokens_table(
+  database: Database<sqlite3.Database, sqlite3.Statement>
+) {
+  await database.exec(`
+    CREATE TABLE IF NOT EXISTS honeytokens (
+      token_id VARCHAR PRIMARY KEY,
+      group_id VARCHAR,
+      type_id INTEGER,
+      creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+      expire_date DATETIME,
+      notes TEXT,
+      data TEXT,
+      FOREIGN KEY (type_id) REFERENCES types(id)
+    );
+  `);
+}
 
 export async function startDatabase() {
   try {
-    const database = await open({
+    const database: Database<sqlite3.Database, sqlite3.Statement> = await open({
       filename: "./database.sqlite",
       driver: sqlite3.Database,
     });
+
+    await init_honeytokens_table(database);
+
     return database;
   } catch (error) {
     console.error("Failed to initialize database:", error);

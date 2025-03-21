30ca873f972973f157b373586bb7b08eb399a85b
Maya Zirin <mayazi1503@gmail.com>
2025-03-21 17:19:48 +0200

    header component update

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,25 @@
 import "../styles/Header.css";
+import logo from "../assets/logo.png";
 
 function Header() {
 	return (
 		<header className="header">
 			<nav>
+				<div>
+                    <img src={logo} alt="Logo" className="logo" />
+                </div>
 				<ul>
 					<li>
 						<a href="/">Home</a>
 					</li>
 					<li>
 						<a href="/alerts">Alerts</a>
+					</li>
+					<li>
+						<a href="/tokens">Tokens</a>
+					</li>
+					<li>
+						<a href="/create">Create</a>
 					</li>
 				</ul>
 				<ul>

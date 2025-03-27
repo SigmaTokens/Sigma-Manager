1d9e4d78339972ab1a5556f5b4dc38ffd4e7b8f3
Daniel Nagornov <Danieln056@gmail.com>
2025-03-27 15:08:01 +0200

    move server code to a main function and execute it

--- server/index.ts
+++ server/index.ts
@@ -3,35 +3,47 @@ import cors from "cors";
 import { serveClient } from "./routes/client";
 import { serveHoneytokens } from "./routes/honeytokens";
 import { serveAlerts } from "./routes/alerts";
+import { isAdmin } from "./utilities/auth";
 import { startDatabase } from "../database/database";
 
-const app = express();
-app.use(cors());
-const port = process.env.PORT || 3000;
+main();
 
-startDatabase()
-	.then((database) => {
-		app.locals.db = database;
-		console.log("[+] Database connection initialized:", app.locals.db);
+function main(): void {
+	const app = express();
+	app.use(cors());
+	const port = process.env.PORT || 3000;
 
-		serveHoneytokens(app, database);
-		serveAlerts(app);
-		serveClient(app);
+	isAdmin().then((isAdmin) => {
+		if (!isAdmin) {
+			console.error("Please run as administrator");
+		}
 
-		test_honeytoken();
+		startDatabase()
+			.then((database) => {
+				app.locals.db = database;
+				console.log("[+] Database connection initialized:", app.locals.db);
 
-		app.listen(port, () => {
-			console.log(`[+] Server running on port ${port}`);
-		});
-	})
-	.catch((error) => {
-		console.error("[-] Failed to initialize server:", error);
-		process.exit(1);
+				serveHoneytokens(app, database);
+				serveAlerts(app);
+				serveClient(app);
+
+				// test_honeytoken();
+
+				app.listen(port, () => {
+					console.log(`[+] Server running on port ${port}`);
+				});
+			})
+			.catch((error) => {
+				console.error("[-] Failed to initialize server:", error);
+				process.exit(1);
+			});
 	});
-
-import { Honeytoken_Text } from "./classes/Honeytoken_Text";
-
-function test_honeytoken(): void {
-	let ht_t = new Honeytoken_Text("1", "1", "text", "C:\\Users\\danie\\Desktop\\test.txt");
-	ht_t.startAgent();
 }
+
+// import { Honeytoken_Text } from "./classes/Honeytoken_Text";
+// import isElevated from "is-elevated";
+
+// function test_honeytoken(): void {
+// 	let ht_t = new Honeytoken_Text("1", "1", "text", "C:\\Users\\danie\\Desktop\\test.txt");
+// 	ht_t.startAgent();
+// }

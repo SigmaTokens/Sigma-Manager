0d4601e9a49ba13252e70bb12a934a35a59a30a7
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2025-04-02 22:57:43 +0300

    make prettier as default vscode formatter and auto re-format on save!

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'url'
 import { execSync, exec } from 'child_process'
 import fs from 'fs'
 import path from 'path'
@@ -7,6 +8,9 @@ main()
 function main() {
   const mode = get_mode()
   init_database_file()
+  const root_dir = get_root_dir()
+  setup_prettier_config(root_dir)
+  setup_vscode_settings(root_dir)
   install_extensions()
   install_deps()
   run_sigmatokens(mode)
@@ -19,7 +23,7 @@ function get_mode() {
     : mode.includes('prod')
       ? 'prod'
       : (console.error(
-          '[-] Please specify a mode to run the project: dev or prod'
+          '[-] Please specify a mode to run the project: dev or prod',
         ),
         process.exit(-1))
 }
@@ -34,6 +38,12 @@ function init_database_file() {
   console.log(`[+] Database absolute path: ${database_path}`)
 }
 
+function get_root_dir() {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  return __dirname
+}
+
 function is_extension_installed(extension) {
   const listCommand = 'code --list-extensions'
   const installedExtensions = execSync(listCommand).toString().split('\n')
@@ -46,7 +56,7 @@ function is_extension_updated(extension) {
       `code --list-extensions --show-versions | grep ${extension}`,
       (error, stdout) => {
         resolve(stdout.includes('@') ? stdout.trim() : null)
-      }
+      },
     )
   })
 }
@@ -78,6 +88,49 @@ function install_extensions() {
   const prettier = 'esbenp.prettier-vscode'
   install_extension(prettier)
   console.log('[+] Extension check complete!')
+}
+
+function create_file(filePath, content) {
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, content, 'utf8')
+    console.log(`[+] Created: ${filePath}`)
+  } else {
+    console.log(`[+] Already Exists: ${filePath}`)
+  }
+}
+
+function setup_prettier_config(rootDir) {
+  const prettierConfig = `{
+    "singleQuote": true,
+    "trailingComma": "all",
+    "tabWidth": 2,
+    "semi": false
+  }`
+
+  const prettierIgnore = `node_modules
+dist
+build
+database/*.json
+`
+
+  create_file(path.join(rootDir, '.prettierrc'), prettierConfig)
+  create_file(path.join(rootDir, '.prettierignore'), prettierIgnore)
+}
+
+function setup_vscode_settings(rootDir) {
+  const vscodeDir = path.join(rootDir, '.vscode')
+  const settingsFile = path.join(vscodeDir, 'settings.json')
+
+  if (!fs.existsSync(vscodeDir)) {
+    fs.mkdirSync(vscodeDir)
+  }
+
+  const settings = {
+    'editor.defaultFormatter': 'esbenp.prettier-vscode',
+    'editor.formatOnSave': true,
+  }
+
+  create_file(settingsFile, JSON.stringify(settings, null, 2))
 }
 
 function install_deps() {

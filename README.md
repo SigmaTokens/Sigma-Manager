36bbdd7d993dbd70f488e1aab30cb7e85a16b35b
Daniel Nagornov <danieln056@gmail.com>
2025-04-15 09:28:02 +0300

    remove unecessary code, add readme information

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Sigma-Manager
 
+## Participants
+
+1. Daniel Nagornov
+2. Daniel Adin
+3. Shaked S.
+4. Maya Zirin
+5. Danielle Menadeva
+
+## Requierments
+
+Please make sure you have node.js installed on your system version >= `v22.14.0`
+
 ## How to run the application
 
 ### Production
@@ -18,10 +30,8 @@ Then you can browse to [localhost:5173](localhost:5173)
 
 Server is running in [localhost:3000](localhost:3000)
 
-## Participants
+## Troubleshooting
 
-1. Daniel Nagornov
-2. Daniel Adin
-3. Shaked S.
-4. Maya Zirin
-5. Danielle Menadeva
+### Windows: "running scripts is disabled on this system"
+
+Please run the following command in powershell: `Set-ExecutionPolicy -Scope CurrentUser RemoteSigned`
--- server/index.ts
+++ server/index.ts
@@ -1,81 +1,47 @@
-import express from "express";
-import cors from "cors";
-import { serveClient } from "./routes/client";
-import { serveHoneytokens } from "./routes/honeytokens";
-import { serveAlerts } from "./routes/alerts";
-import { isAdmin } from "./utilities/auth";
-import { startDatabase } from "../database/database";
-import { Constants } from "./constants";
-import { isWindows, windows_enable_auditing, isMac } from "./utilities/host";
+import express from 'express'
+import cors from 'cors'
+import { serveClient } from './routes/client'
+import { serveHoneytokens } from './routes/honeytokens'
+import { serveAlerts } from './routes/alerts'
+import { isAdmin } from './utilities/auth'
+import { startDatabase } from '../database/database'
+import { Constants } from './constants'
+import { isWindows, windows_enable_auditing, isMac } from './utilities/host'
+import { Globals } from './globals'
 
-main();
+main()
 
 function main(): void {
-  const app = express();
-  app.use(express.json());
+  const app = express()
+  app.use(express.json())
 
-  app.use(cors());
-  app.use(express.urlencoded({ extended: true }));
-  const port = process.env.PORT || 3000;
+  app.use(cors())
+  app.use(express.urlencoded({ extended: true }))
+  const port = process.env.PORT || 3000
 
   isAdmin().then((isAdmin) => {
     if (!isAdmin) {
-      console.error(Constants.TEXT_RED_COLOR, "Please run as administrator");
-      return;
+      console.error(Constants.TEXT_RED_COLOR, 'Please run as administrator')
+      return
     }
-    init().then(() => {
-      startDatabase()
-        .then((database) => {
-          app.locals.db = database;
-          console.log("[+] Database connection initialized:", app.locals.db);
+    startDatabase()
+      .then((database) => {
+        app.locals.db = database
+        console.log('[+] Database connection initialized:', app.locals.db)
 
-          serveHoneytokens(app, database);
-          serveAlerts(app);
-          serveClient(app);
+        serveHoneytokens(app, database)
+        serveAlerts(app)
+        serveClient(app)
 
-          Globals.app = app;
+        Globals.app = app
 
-          //test_honeytoken();
-
-          app.listen(port, () => {
-            console.log(`[+] Server running on port ${port}`);
-          });
+        app.listen(port, () => {
+          console.log(`[+] Server running on port ${port}`)
         })
-        .catch((error) => {
-          console.error("[-] Failed to initialize server:", error);
-          process.exit(1);
-        });
-    });
-  });
-}
-
-async function init() {
-  if (isWindows()) {
-    await windows_enable_auditing();
-  } else if (isMac()) {
-    console.log("Running on Mac");
-  }
-}
-
-import { Honeytoken_Text } from "./classes/Honeytoken_Text";
-import { Globals } from "./globals";
-
-function test_honeytoken(): void {
-  let location = "C:\\Users\\danie\\Desktop";
-  let file_name = "test.txt"; // CHANGE THIS - create a new file first
-  if (isMac()) {
-    location = "/Users/sh/Desktop/";
-    file_name = "a.txt";
-  }
-  let ht_t = new Honeytoken_Text(
-    "1",
-    "1",
-    "text",
-    new Date(),
-    5,
-    "help",
-    location,
-    file_name
-  );
-  ht_t.startAgent();
+      })
+      .catch((error) => {
+        console.error('[-] Failed to initialize server:', error)
+        process.exit(1)
+      })
+  })
 }

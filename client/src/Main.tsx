a650ab379a50ea2cc738eda5080d50483bedd169
Daniel Nagornov <Danieln056@gmail.com>
2025-06-11 21:28:42 +0300

    rename UserProvider -> UserContextProvider

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './contexts/UserContext';
+import { UserContextProvider } from './contexts/UserContext';
 import Header from './components/Header';
 import AppRoutes from './pages/AppRoutes';
 
@@ -9,11 +9,11 @@ import './styles/Main.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <UserProvider>
+    <UserContextProvider>
       <BrowserRouter>
         <Header />
         <AppRoutes />
       </BrowserRouter>
-    </UserProvider>
+    </UserContextProvider>
   </StrictMode>,
 );
